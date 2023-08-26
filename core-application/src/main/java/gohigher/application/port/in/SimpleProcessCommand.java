4311068ab1ebdcf80f639a05a98d4f2b16401334
Seongwon97 <osw970325@naver.com>
2023-08-26 17:19:35 +0900

    feat: 간편 지원 입력 객체 구현

---
+++ core-application/src/main/java/gohigher/application/port/in/SimpleProcessCommand.java
@@ -0,0 +1,22 @@
+package gohigher.application.port.in;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import gohigher.common.ProcessType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SimpleProcessCommand {
+
+	private String type;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm", timezone = "Asia/Seoul")
+	private LocalDateTime schedule;
+
+	public ProcessType getType() {
+		return ProcessType.from(type);
+	}
+}
