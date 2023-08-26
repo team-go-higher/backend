b4f291bb459f415864246edce923370a6a8d9796
Seongwon97 <osw970325@naver.com>
2023-08-26 14:34:56 +0900

    feat: 공고 레포지터리 생성

---
+++ out-adapter-persistence-jpa/src/main/java/gohigher/recruitment/RecruitmentRepository.java
@@ -0,0 +1,6 @@
+package gohigher.recruitment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentRepository extends JpaRepository<RecruitmentJpaEntity, Long> {
+}
