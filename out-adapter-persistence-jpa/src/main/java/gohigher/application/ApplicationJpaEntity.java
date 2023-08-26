96ee57a0de9821b16ff0de1b78f780a4639d6ce9
SeongWon Oh <osw970325@naver.com>
2023-08-26 16:31:52 +0900

    [FEATURE] 공고, 지원 테이블 설계 및 엔티티 구현 (#7)
    * build: out-adapter-persistence에 domain의존성 추가
    
    * feat: 공고 엔티티 생성
    
    * feat: 지원 엔티티 생성
    
    * feat: 공고 레포지터리 생성
    
    * feat: 지원 레포지터리 생성
    
    * build: 로컬 데이터베이스 설정 추가
    
    * build: in-adapter-api에 persistence의존성 추가
    
    * feat: Process의 순서 컬럼 추가
    
    * feat: Application에 현재 프로세스 컬럼 추가
    
    * refactor: order컬럼명을 orders로 변경
    
    * style: 설정 파일의 공백값 추가
    
    * feat: Process에 상세 설명 정보 추가

---
+++ out-adapter-persistence-jpa/src/main/java/gohigher/application/ApplicationJpaEntity.java
@@ -0,0 +1,58 @@
+package gohigher.application;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import gohigher.recruitment.EmploymentType;
+import gohigher.recruitment.RecruitmentJpaEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "application")
+@Entity
+public class ApplicationJpaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private Long userId;
+	private String companyName;
+	private String location;
+	private String contact;
+	private String duty;
+	private String jobDescription;
+	private String workType;
+
+	@Enumerated(value = EnumType.STRING)
+	private EmploymentType employmentType;
+	private String careerRequirement;
+	private String requiredCapability;
+	private String preferredQualification;
+	private LocalDateTime deadline;
+	private String url;
+	private int currentProcess;
+
+	@OneToMany(mappedBy = "application")
+	private List<ApplicationProcessJpaEntity> processes;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "recruitment_id")
+	private RecruitmentJpaEntity recruitment;
+
+	private boolean deleted;
+}
---
+++ out-adapter-persistence-jpa/src/main/java/gohigher/recruitment/RecruitmentProcessJpaEntity.java
@@ -0,0 +1,41 @@
+package gohigher.recruitment;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "recruitment_process")
+@Entity
+public class RecruitmentProcessJpaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "recruitment_id", nullable = false)
+	private RecruitmentJpaEntity recruitment;
+
+	@Column(nullable = false)
+	@Enumerated(value = EnumType.STRING)
+	private ProcessType type;
+
+	private int orders;
+	private LocalDateTime schedule;
+	private String description;
+}
