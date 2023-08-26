5210c42950cb0c280bfb0cca451ee627b3fc92fd
Seongwon97 <osw970325@naver.com>
2023-08-26 15:18:04 +0900

    refactor: 지원서와 공고 클래스의 분리

--- core-domain/src/main/java/gohigher/application/Application.java
+++ core-domain/src/main/java/gohigher/application/Application.java
@@ -1,9 +1,25 @@
 package gohigher.application;
 
-import gohigher.recruitment.Recruitment;
+import java.time.LocalDateTime;
+import java.util.List;
 
-public record Application(
-        Recruitment recruitment,
-        int currentProcess
-) {
+import gohigher.common.EmploymentType;
+import gohigher.common.JobInfo;
+import gohigher.common.Process;
+
+/**
+ * currentProcess   현재 프로세스
+ */
+public class Application extends JobInfo {
+
+	private final int currentProcess;
+
+	public Application(String companyName, String location, String contact, String duty, String jobDescription,
+		String workType, EmploymentType employmentType, String careerRequirement, String requiredCapability,
+		String preferredQualification, LocalDateTime deadLine, List<Process> processes,
+		String url, int currentProcess) {
+		super(companyName, location, contact, duty, jobDescription, workType, employmentType, careerRequirement,
+			requiredCapability, preferredQualification, deadLine, processes, url);
+		this.currentProcess = currentProcess;
+	}
 }
