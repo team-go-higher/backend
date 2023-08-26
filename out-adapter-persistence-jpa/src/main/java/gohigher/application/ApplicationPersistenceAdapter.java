55cabaf9438ebc9e6d49ae47a3dba2f2bc62a15d
Seongwon97 <osw970325@naver.com>
2023-08-26 18:19:45 +0900

    feat: 지원 저장 out adapter 구현

---
+++ out-adapter-persistence-jpa/src/main/java/gohigher/application/ApplicationPersistenceAdapter.java
@@ -0,0 +1,37 @@
+package gohigher.application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import gohigher.application.entity.ApplicationJpaEntity;
+import gohigher.application.entity.ApplicationProcessJpaEntity;
+import gohigher.application.entity.ApplicationProcessRepository;
+import gohigher.application.entity.ApplicationRepository;
+import gohigher.application.port.out.persistence.ApplicationPersistencePort;
+import gohigher.common.Process;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationPersistenceAdapter implements ApplicationPersistencePort {
+
+	private final ApplicationRepository applicationRepository;
+	private final ApplicationProcessRepository applicationProcessRepository;
+
+	@Override
+	public Long save(Long userId, Application application) {
+		ApplicationJpaEntity applicationJpaEntity =
+			applicationRepository.save(ApplicationJpaEntity.from(userId, application));
+		List<ApplicationProcessJpaEntity> applicationProcessJpaEntities = new ArrayList<>();
+		List<Process> processes = application.getProcesses();
+		for (int i = 0; i < processes.size(); i++) {
+			applicationProcessJpaEntities.add(
+				ApplicationProcessJpaEntity.from(applicationJpaEntity, processes.get(i), i));
+		}
+		applicationProcessRepository.saveAll(applicationProcessJpaEntities);
+
+		return applicationJpaEntity.getId();
+	}
+}
