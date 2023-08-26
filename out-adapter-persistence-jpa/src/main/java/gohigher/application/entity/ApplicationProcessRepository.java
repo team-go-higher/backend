9bfac18a9da882fe19b4d7a78810e8f320bfadfd
Seongwon97 <osw970325@naver.com>
2023-08-26 18:00:33 +0900

    feat: ApplicationProcessRepository 생성

---
+++ out-adapter-persistence-jpa/src/main/java/gohigher/application/entity/ApplicationProcessRepository.java
@@ -0,0 +1,6 @@
+package gohigher.application.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationProcessRepository extends JpaRepository<ApplicationProcessJpaEntity, Long> {
+}
