670872bec10bbcff29eed9feab604acde16a6d87
Seongwon97 <osw970325@naver.com>
2023-08-26 17:19:04 +0900

    feat: ProcessType 정적 팩터리 메서드 구현

--- core-domain/src/main/java/gohigher/common/ProcessType.java
+++ core-domain/src/main/java/gohigher/common/ProcessType.java
@@ -1,9 +1,19 @@
 package gohigher.common;
 
+import java.util.Arrays;
+
 public enum ProcessType {
 	TO_APPLY,
 	DOCUMENT,
 	TEST,
 	INTERVIEW,
 	COMPLETE,
+	;
+
+	public static ProcessType from(String value) {
+		return Arrays.stream(values())
+			.filter(processType -> processType.name().equals(value))
+			.findAny()
+			.orElseThrow(IllegalArgumentException::new);
+	}
 }
