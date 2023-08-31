caecf3270b24164f7ee306eb4289fea247ba902f
Seongwon97 <osw970325@naver.com>
2023-08-31 11:00:22 +0900

    feat: 서비스 내부 예외인 GoHigherException 생성

---
+++ core-domain/src/main/java/gohigher/global/exception/GoHigherException.java
@@ -0,0 +1,17 @@
+package gohigher.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class GoHigherException extends RuntimeException {
+
+	private final int statusCode;
+	private final String errorCode;
+	private final String message;
+
+	public GoHigherException(ErrorCode code) {
+		statusCode = code.getStatusCode();
+		errorCode = code.getErrorCode();
+		message = code.getMessage();
+	}
+}
