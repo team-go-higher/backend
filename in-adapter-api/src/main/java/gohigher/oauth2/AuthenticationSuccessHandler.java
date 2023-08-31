15600ad6552de9f2693f2933de7e0e6f3a1ad30c
shindong96 <2006peter@naver.com>
2023-08-31 23:55:19 +0900

    feat: Guest가 아닌 사용자 로그인 시 리프레시 토큰 발급

--- in-adapter-api/src/main/java/gohigher/oauth2/AuthenticationSuccessHandler.java
+++ in-adapter-api/src/main/java/gohigher/oauth2/AuthenticationSuccessHandler.java
@@ -1,15 +1,19 @@
 package gohigher.oauth2;
 
+import static org.springframework.http.HttpHeaders.*;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import gohigher.support.CookieProvider;
 import gohigher.support.JwtProvider;
 import gohigher.user.Role;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,12 +25,15 @@ import lombok.RequiredArgsConstructor;
 public class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 	private final JwtProvider jwtProvider;
+	private final CookieProvider cookieProvider;
 
+	@SuppressWarnings("checkstyle:WhitespaceAround")
 	@Override
 	public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response,
 		Authentication authentication) throws IOException {
 		OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
 
+		String email = oAuth2User.getAttribute("email");
 		String role = oAuth2User.getAuthorities().stream()
 			.findFirst()
 			.orElseThrow(IllegalAccessError::new) // 존재하지 않을 시 예외를 던진다.
@@ -35,14 +42,30 @@ public class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccess
 		String requestUri = request.getRequestURI();
 		Date now = new Date();
 
-		String accessToken = jwtProvider.createAccessToken(oAuth2User.getAttribute("email"), now);
+		String accessToken = jwtProvider.createAccessToken(email, now);
+		String targetUrl = createTargetUrl(role, requestUri, accessToken);
+		addRefreshTokenCookie(response, email, role, now);
+
+		getRedirectStrategy().sendRedirect(request, response, targetUrl);
+	}
+
+	private String createTargetUrl(String role, String requestUri, String accessToken) {
 		String targetUrl = UriComponentsBuilder.fromUriString(requestUri)
 			.queryParam("accessToken", accessToken)
 			.queryParam("role", Role.valueOf(role).toString())
 			.build()
 			.encode(StandardCharsets.UTF_8)
 			.toUriString();
+		return targetUrl;
+	}
 
-		getRedirectStrategy().sendRedirect(request, response, targetUrl);
+	private void addRefreshTokenCookie(HttpServletResponse response, String email, String role, Date now) {
+		if (role.equals(Role.GUEST.toString())) {
+			return;
+		}
+
+		String refreshToken = jwtProvider.createRefreshToken(email, now);
+		ResponseCookie responseCookie = cookieProvider.create(refreshToken);
+		response.addHeader(SET_COOKIE, responseCookie.toString());
 	}
 }
---
+++ in-adapter-api/src/main/java/gohigher/support/CookieProvider.java
@@ -0,0 +1,33 @@
+package gohigher.support;
+
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.Cookie.SameSite;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieProvider {
+
+	private static final String REFRESH_TOKEN = "refreshToken";
+	private final long refreshTokenExpireLength;
+
+	public CookieProvider(@Value("$security.jwt.expire-length.refresh") long refreshTokenExpireLength) {
+		this.refreshTokenExpireLength = refreshTokenExpireLength;
+	}
+
+	public ResponseCookie create(final String refreshToken) {
+		return createTokenCookieBuilder(refreshToken)
+			.maxAge(Duration.ofMillis(refreshTokenExpireLength))
+			.build();
+	}
+
+	private ResponseCookie.ResponseCookieBuilder createTokenCookieBuilder(final String value) {
+		return ResponseCookie.from(REFRESH_TOKEN, value)
+			.httpOnly(true)
+			.secure(true)
+			.path("/token")
+			.sameSite(SameSite.NONE.attributeValue());
+	}
+}
