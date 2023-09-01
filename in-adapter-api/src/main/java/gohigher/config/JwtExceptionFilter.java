8482aacfc35c2d30c47c4a5c207a67da3ebf589f
shindong96 <2006peter@naver.com>
2023-09-01 15:33:29 +0900

    feat: 토큰 만료시 401 응답

---
+++ in-adapter-api/src/main/java/gohigher/config/JwtExceptionFilter.java
@@ -0,0 +1,28 @@
+package gohigher.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtExceptionFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+		throws ServletException, IOException {
+		try {
+			filterChain.doFilter(request, response);
+		} catch (JwtException e) {
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+		}
+	}
+}
