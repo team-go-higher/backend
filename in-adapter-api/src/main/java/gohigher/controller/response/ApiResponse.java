83b37ce5d89f9728edb421b10dc303f92ccdb600
Seongwon97 <osw970325@naver.com>
2023-09-02 15:36:36 +0900

    feat: ApiResponse에 접근을 위한 Getter 추가

--- in-adapter-api/src/main/java/gohigher/controller/response/ApiResponse.java
+++ in-adapter-api/src/main/java/gohigher/controller/response/ApiResponse.java
@@ -1,8 +1,10 @@
 package gohigher.controller.response;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
 
