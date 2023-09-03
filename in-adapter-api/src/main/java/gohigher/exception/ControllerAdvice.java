64d034974d61323e33863a08e0cad7ac1d659f7f
Seongwon97 <osw970325@naver.com>
2023-09-03 10:41:53 +0900

    feat: 다루어지지 않은 에러에 대한 처리 추가

--- in-adapter-api/src/main/java/gohigher/exception/ControllerAdvice.java
+++ in-adapter-api/src/main/java/gohigher/exception/ControllerAdvice.java
@@ -5,15 +5,30 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import gohigher.controller.response.ApiResponse;
+import gohigher.global.exception.ErrorCodeFinder;
+import gohigher.global.exception.GlobalErrorCode;
 import gohigher.global.exception.GoHigherException;
+import lombok.RequiredArgsConstructor;
 
 @RestControllerAdvice
+@RequiredArgsConstructor
 public class ControllerAdvice {
+
+	private final ErrorCodeFinder errorCodeFinder;
 
 	@ExceptionHandler(GoHigherException.class)
 	public ResponseEntity<ApiResponse<Void>> handleGoHigherException(GoHigherException e) {
 		int statusCode = e.getStatusCode();
 		ApiResponse<Void> response = ApiResponse.fail(e.getErrorCode(), e.getMessage());
+
+		return ResponseEntity.status(statusCode).body(response);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ApiResponse<Void>> uncontrolledException(Exception e) {
+		GlobalErrorCode errorCode = GlobalErrorCode.NOT_CONTROLLED_ERROR;
+		int statusCode = errorCode.getStatusCode();
+		ApiResponse<Void> response = ApiResponse.fail(errorCode.getErrorCode(), errorCode.getMessage());
 
 		return ResponseEntity.status(statusCode).body(response);
 	}
