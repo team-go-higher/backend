ac123d1fe2866a519c3d8eb32b4531fd368e335b
Chris <bjuuuu98@gmail.com>
2023-09-04 15:46:02 +0900

    feat: 지원서 간편 등록 API 구현

---
+++ in-adapter-api/src/main/java/gohigher/application/ApplicationCommandController.java
@@ -0,0 +1,27 @@
+package gohigher.application;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import gohigher.application.port.in.ApplicationCommandPort;
+import gohigher.application.port.in.SimpleApplicationCommand;
+import gohigher.auth.support.AuthenticationPrincipal;
+import gohigher.controller.response.GohigherResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ApplicationCommandController implements ApplicationCommandControllerDocs {
+
+	private final ApplicationCommandPort applicationCommandPort;
+
+	@PostMapping
+	public ResponseEntity<GohigherResponse<Void>> registerApplication(@AuthenticationPrincipal Long userId,
+		@RequestBody @Valid SimpleApplicationCommand command) {
+		applicationCommandPort.applySimply(userId, command);
+		return ResponseEntity.ok(GohigherResponse.success(null));
+	}
+}
