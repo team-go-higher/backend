ec5af1b20f9c92ed746e45240335b7b4994231d3
Chris <bjuuuu98@gmail.com>
2023-09-04 15:46:12 +0900

    docs: 지원서 간편 등록 API 문서화

---
+++ in-adapter-api/src/main/java/gohigher/application/ApplicationCommandControllerDocs.java
@@ -0,0 +1,25 @@
+package gohigher.application;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import gohigher.application.port.in.SimpleApplicationCommand;
+import gohigher.controller.response.GohigherResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "지원서")
+public interface ApplicationCommandControllerDocs {
+
+	@Operation(summary = "지원서 간편등록")
+	@ApiResponses(
+		value = {@ApiResponse(responseCode = "200", description = "간편 지원서 등록 성공"),
+			@ApiResponse(responseCode = "400", description = "회사명 혹은 직무가 입력되지 않음")
+		}
+	)
+	ResponseEntity<GohigherResponse<Void>> registerApplication(@Parameter(hidden = true) Long userId,
+		@RequestBody SimpleApplicationCommand command);
+}
