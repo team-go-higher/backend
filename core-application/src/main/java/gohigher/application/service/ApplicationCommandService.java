ef852ee17bd76f925333d8bb1ba34a6f64c904ae
Chris <bjuuuu98@gmail.com>
2023-09-05 09:42:22 +0900

    feat: 간편 지원 기능 유스케이스 구현

---
+++ core-application/src/main/java/gohigher/application/service/ApplicationCommandService.java
@@ -0,0 +1,22 @@
+package gohigher.application.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import gohigher.application.port.in.ApplicationCommandPort;
+import gohigher.application.port.in.SimpleApplicationCommand;
+import gohigher.application.port.out.persistence.ApplicationPersistenceCommandPort;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ApplicationCommandService implements ApplicationCommandPort {
+
+	private final ApplicationPersistenceCommandPort applicationPersistenceCommandPort;
+
+	@Override
+	public void applySimply(Long userId, SimpleApplicationCommand command) {
+		applicationPersistenceCommandPort.save(userId, command.toDomain());
+	}
+}
