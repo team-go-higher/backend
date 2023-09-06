8b5ed3611255e7544e4b67159e0a38ee0f5410bd
Seongwon97 <osw970325@naver.com>
2023-09-06 17:21:11 +0900

    feat: 지원서 월별 조회 서비스 로직 구현

---
+++ core-application/src/main/java/gohigher/application/service/ApplicationQueryService.java
@@ -0,0 +1,41 @@
+package gohigher.application.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import gohigher.application.Application;
+import gohigher.application.port.in.ApplicationCalendarResponse;
+import gohigher.application.port.in.ApplicationMonthQueryResponse;
+import gohigher.application.port.in.ApplicationQueryPort;
+import gohigher.application.port.out.persistence.ApplicationPersistenceQueryPort;
+import gohigher.common.Process;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ApplicationQueryService implements ApplicationQueryPort {
+
+	private final ApplicationPersistenceQueryPort applicationPersistenceQueryPort;
+
+	@Override
+	public ApplicationMonthQueryResponse findByMonth(Long userId, int month) {
+		List<Application> applications = applicationPersistenceQueryPort.findByIdAndMonth(userId, month);
+
+		List<ApplicationCalendarResponse> applicationCalendarResponses = new ArrayList<>();
+		for (Application application : applications) {
+			addScheduleResponses(application, applicationCalendarResponses);
+		}
+		return new ApplicationMonthQueryResponse(applicationCalendarResponses);
+	}
+
+	private void addScheduleResponses(Application application,
+		List<ApplicationCalendarResponse> applicationCalendarResponses) {
+		for (Process process : application.getProcesses()) {
+			applicationCalendarResponses.add(
+				new ApplicationCalendarResponse(application.getCompanyName(), process.getType().name(),
+					process.getSchedule()));
+		}
+	}
+}
