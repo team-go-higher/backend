2dcadb2e6f412e14e3e305cf2c0ee0543030fb50
Seongwon97 <osw970325@naver.com>
2023-09-06 16:23:10 +0900

    feat: 캘린더 월별 조회 out port 정의

---
+++ core-application/src/main/java/gohigher/application/port/out/persistence/ApplicationPersistenceQueryPort.java
@@ -0,0 +1,10 @@
+package gohigher.application.port.out.persistence;
+
+import java.util.List;
+
+import gohigher.application.Application;
+
+public interface ApplicationPersistenceQueryPort {
+
+	List<Application> findByIdAndMonth(Long userId, int month);
+}
