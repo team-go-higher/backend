19f8b6d1c6f9768cb1682edfb9f11aa038a5d231
Chris <bjuuuu98@gmail.com>
2023-09-08 17:34:40 +0900

    feat: 유효하지 않은 고용 형태 값 에러 코드 정의

--- core-domain/src/main/java/gohigher/common/JobInfoErrorCode.java
+++ core-domain/src/main/java/gohigher/common/JobInfoErrorCode.java
@@ -11,6 +11,7 @@ public enum JobInfoErrorCode implements ErrorCode {
 	INVALID_PROCESS_TYPE(400, "JOB_INFO_001", "유효하지 않은 전형 단계입니다."),
 	COMPANY_NAME_BLANK(400, "JOB_INFO_002", "회사명이 입력되지 않았습니다."),
 	DUTY_BLANK(400, "JOB_INFO_003", "직무가 입력되지 않았습니다."),
+	INVALID_EMPLOYMENT_TYPE(400, "JOB_INFO_004", "유효하지 않은 고용 형태입니다.")
 	;
 
 	private final int statusCode;
