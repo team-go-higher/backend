24860be35eb48c2126b8bc2066d1261233be65fe
Chris <bjuuuu98@gmail.com>
2023-09-08 23:18:06 +0900

    feat: 지원서 현재 절차 변경 db out-port 정의

--- core-application/src/main/java/gohigher/application/port/out/persistence/ApplicationPersistenceCommandPort.java
+++ core-application/src/main/java/gohigher/application/port/out/persistence/ApplicationPersistenceCommandPort.java
@@ -1,8 +1,11 @@
 package gohigher.application.port.out.persistence;
 
 import gohigher.application.Application;
+import gohigher.common.Process;
 
 public interface ApplicationPersistenceCommandPort {
 
 	Long save(Application application);
+
+	void updateCurrentProcess(Long id, Process process);
 }
