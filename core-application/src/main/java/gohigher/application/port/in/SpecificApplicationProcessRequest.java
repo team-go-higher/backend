1d6d76195c3e093e9e4c751d493e46f0d538a2c5
Chris <bjuuuu98@gmail.com>
2023-09-08 18:31:48 +0900

    feat: 지원서 상세 등록시 전형 정보 입력 값 예외 처리

--- core-application/src/main/java/gohigher/application/port/in/SpecificApplicationProcessRequest.java
+++ core-application/src/main/java/gohigher/application/port/in/SpecificApplicationProcessRequest.java
@@ -4,11 +4,16 @@ import java.time.LocalDateTime;
 
 import gohigher.common.Process;
 import gohigher.common.ProcessType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public class SpecificApplicationProcessRequest {
 
+	@NotBlank(message = "JOB_INFO_005||전형 단계가 입력되지 않았습니다.")
 	private String type;
+	@NotBlank(message = "JOB_INFO_006||세부 전형이 입력되지 않았습니다.")
 	private String description;
+	@NotNull(message = "JOB_INFO_007||전형 일정이 입력되지 않았습니다.")
 	private LocalDateTime schedule;
 
 	public Process toDomain() {
--- core-domain/src/main/java/gohigher/common/JobInfoErrorCode.java
+++ core-domain/src/main/java/gohigher/common/JobInfoErrorCode.java
@@ -11,7 +11,10 @@ public enum JobInfoErrorCode implements ErrorCode {
 	INVALID_PROCESS_TYPE(400, "JOB_INFO_001", "유효하지 않은 전형 단계입니다."),
 	COMPANY_NAME_BLANK(400, "JOB_INFO_002", "회사명이 입력되지 않았습니다."),
 	DUTY_BLANK(400, "JOB_INFO_003", "직무가 입력되지 않았습니다."),
-	INVALID_EMPLOYMENT_TYPE(400, "JOB_INFO_004", "유효하지 않은 고용 형태입니다.")
+	INVALID_EMPLOYMENT_TYPE(400, "JOB_INFO_004", "유효하지 않은 고용 형태입니다."),
+	PROCESS_TYPE_BLANK(400, "JOB_INFO_005", "전형 단계가 입력되지 않았습니다."),
+	PROCESS_DESCRIPTION_BLANK(400, "JOB_INFO_006", "세부 전형이 입력되지 않았습니다."),
+	PROCESS_SCHEDULE_NULL(400, "JOB_INFO_007", "전형 일정이 입력되지 않았습니다."),
 	;
 
 	private final int statusCode;
