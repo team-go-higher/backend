e2f369417437c3be5fdbe3642fa237412e7e9865
Chris <bjuuuu98@gmail.com>
2023-09-08 22:38:02 +0900

    feat: 지원서의 전형 절차 NotFound 에러 코드 정의

--- core-domain/src/main/java/gohigher/application/ApplicationErrorCode.java
+++ core-domain/src/main/java/gohigher/application/ApplicationErrorCode.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum ApplicationErrorCode implements ErrorCode {
 
 	APPLICATION_NOT_FOUND(400, "APPLICATION_001", "존재하지 않는 지원서입니다."),
+	APPLICATION_PROCESS_NOT_FOUND(404, "APPLICATION_002", "지원서에서 존재하지 않는 ProcessType 입니다.")
 	;
 
 	private final int statusCode;
