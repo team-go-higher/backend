c0d787c31739316d9fd9f14f96499b62c28a3bcb
Chris <bjuuuu98@gmail.com>
2023-09-08 23:26:19 +0900

    feat: CurrentProcessUpdateRequest 생성자 추가

--- core-application/src/main/java/gohigher/application/port/in/CurrentProcessUpdateRequest.java
+++ core-application/src/main/java/gohigher/application/port/in/CurrentProcessUpdateRequest.java
@@ -1,10 +1,12 @@
 package gohigher.application.port.in;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class CurrentProcessUpdateRequest {
 
 	private Long applicationId;
