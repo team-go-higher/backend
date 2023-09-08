486d6711675c6a81e7cb153c376f9bed0bddefba
Chris <bjuuuu98@gmail.com>
2023-09-08 19:26:26 +0900

    docs: 지원서 상세 등록 API 구현

--- in-adapter-api/src/main/java/gohigher/application/ApplicationCommandControllerDocs.java
+++ in-adapter-api/src/main/java/gohigher/application/ApplicationCommandControllerDocs.java
@@ -4,9 +4,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import gohigher.application.port.in.SimpleApplicationRequest;
+import gohigher.application.port.in.SpecificApplicationRequest;
 import gohigher.controller.response.GohigherResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -22,4 +25,67 @@ public interface ApplicationCommandControllerDocs {
 	)
 	ResponseEntity<GohigherResponse<Void>> registerApplicationSimply(@Parameter(hidden = true) Long userId,
 		@RequestBody SimpleApplicationRequest command);
+
+	@Operation(summary = "지원서 상세등록")
+	@ApiResponses(
+		value = {
+			@ApiResponse(responseCode = "200", description = "상세 지원서 등록 성공"),
+			@ApiResponse(responseCode = "400", description = "상세 지원서 등록 실패", content = @Content(
+				examples = {
+					@ExampleObject(name = "유효하지 않은 전형 단계", value = """
+						{
+						"success": false,
+						"error": {
+							"code": "JOB_INFO_001",
+							"message": "유효하지 않은 고용 형태입니다."
+						},
+						"data": null
+						}
+						"""),
+					@ExampleObject(name = "유효하지 않은 고용 형태", value = """
+						{
+						"success": false,
+						"error": {
+							"code": "JOB_INFO_004",
+							"message": "유효하지 않은 고용 형태입니다."
+						},
+						"data": null
+						}
+						"""),
+					@ExampleObject(name = "전형 단계 입력되지 않음", value = """
+						{
+						"success": false,
+						"error": {
+							"code": "JOB_INFO_005",
+							"message": "전형 단계가 입력되지 않았습니다."
+						},
+						"data": null
+						}
+						"""),
+					@ExampleObject(name = "세부 전형 입력되지 않음", value = """
+						{
+						"success": false,
+						"error": {
+							"code": "JOB_INFO_006",
+							"message": "세부 전형이 입력되지 않았습니다."
+						},
+						"data": null
+						}
+						"""),
+					@ExampleObject(name = "전형 일정 입력되지 않음", value = """
+						{
+						"success": false,
+						"error": {
+							"code": "JOB_INFO_007",
+							"message": "전형 일정이 입력되지 않았습니다."
+						},
+						"data": null
+						}
+						""")
+				}
+			))
+		}
+	)
+	ResponseEntity<GohigherResponse<Void>> registerApplicationSpecifically(@Parameter(hidden = true) Long userId,
+		SpecificApplicationRequest command);
 }
