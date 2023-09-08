f7e7be5a28f11ef7616c37514c09fc48078b85b7
Chris <bjuuuu98@gmail.com>
2023-09-08 17:49:51 +0900

    feat: 지원서 상세 등록 in port 정의

--- core-application/src/main/java/gohigher/application/port/in/ApplicationCommandPort.java
+++ core-application/src/main/java/gohigher/application/port/in/ApplicationCommandPort.java
@@ -3,4 +3,6 @@ package gohigher.application.port.in;
 public interface ApplicationCommandPort {
 
 	void applySimply(Long userId, SimpleApplicationRequest command);
+
+	void applySpecifically(Long userId, SpecificApplicationRequest command);
 }
