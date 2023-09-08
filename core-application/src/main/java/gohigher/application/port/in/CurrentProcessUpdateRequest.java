9e52fdc6dc44918f35aec7182712bd0efb51a3fa
Chris <bjuuuu98@gmail.com>
2023-09-08 21:58:37 +0900

    feat: 현재 채용 프로세스 변경 요청 객체 정의

---
+++ core-application/src/main/java/gohigher/application/port/in/CurrentProcessUpdateRequest.java
@@ -0,0 +1,12 @@
+package gohigher.application.port.in;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CurrentProcessUpdateRequest {
+
+	private Long applicationId;
+	private Long currentProcess;
+}
