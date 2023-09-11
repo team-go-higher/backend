bec8cc51672561fba92c3945e46e9729ed986d5f
Chris <bjuuuu98@gmail.com>
2023-09-12 00:32:36 +0900

    feat: 지원서 현재 전형 변경 요청 입력 값 검증 로직 추가

--- core-application/src/main/java/gohigher/application/port/in/CurrentProcessUpdateRequest.java
+++ core-application/src/main/java/gohigher/application/port/in/CurrentProcessUpdateRequest.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CurrentProcessUpdateRequest {
 
+	@NotNull(message = "APPLICATION_004||지원서 id가 입력되지 않았습니다.")
 	private Long applicationId;
+	@NotNull(message = "APPLICATION_006||지원서의 전형 id가 입력되지 않았습니다.")
 	private Long processId;
 }
