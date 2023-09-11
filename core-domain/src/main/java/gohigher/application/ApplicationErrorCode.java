63974327b49142f9711f44e6ae692a49f695ec82
Chris <bjuuuu98@gmail.com>
2023-09-11 23:58:27 +0900

    refactor: APPLICATION_PROCESS_NOT_FOUND 에러 메시지 변경

--- core-domain/src/main/java/gohigher/application/ApplicationErrorCode.java
+++ core-domain/src/main/java/gohigher/application/ApplicationErrorCode.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum ApplicationErrorCode implements ErrorCode {
 
 	APPLICATION_NOT_FOUND(404, "APPLICATION_001", "존재하지 않는 지원서입니다."),
-	APPLICATION_PROCESS_NOT_FOUND(404, "APPLICATION_002", "지원서에서 존재하지 않는 ProcessType 입니다."),
+	APPLICATION_PROCESS_NOT_FOUND(404, "APPLICATION_002", "지원서에 존재하지 않는 전형입니다."),
 	APPLICATION_FORBIDDEN(403, "APPLICATION_003", "지원서에 대한 수정 권한이 없습니다."),
 	;
 
