a32da2d007b451de3b603c58c3573147e8fea36c
Chris <bjuuuu98@gmail.com>
2023-09-13 22:04:49 +0900

    style: 입력 객체 필드 간 개행 추가

--- core-application/src/main/java/gohigher/application/port/in/CurrentProcessUpdateRequest.java
+++ core-application/src/main/java/gohigher/application/port/in/CurrentProcessUpdateRequest.java
@@ -12,6 +12,7 @@ public class CurrentProcessUpdateRequest {
 
 	@NotNull(message = "APPLICATION_004||지원서 id가 입력되지 않았습니다.")
 	private Long applicationId;
+
 	@NotNull(message = "APPLICATION_005||지원서의 전형 id가 입력되지 않았습니다.")
 	private Long processId;
 }
