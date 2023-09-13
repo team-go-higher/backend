6e0ba55f1f1913165ad11cff3ff22ed40e1c2b87
shindong96 <2006peter@naver.com>
2023-09-13 16:17:13 +0900

    refactor: 딜리미터 상수화

--- in-adapter-api/src/main/java/gohigher/config/AuthConfig.java
+++ in-adapter-api/src/main/java/gohigher/config/AuthConfig.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class AuthConfig implements WebMvcConfigurer {
 
 	private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+	private static final String SPLIT_REGEX = ",";
 
 	private final List<HandlerMethodArgumentResolver> resolvers;
 	private final String[] allowedOrigin;
@@ -29,7 +30,7 @@ public class AuthConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins(allowedOrigin)
-			.allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+			.allowedMethods(ALLOWED_METHOD_NAMES.split(SPLIT_REGEX))
 			.allowCredentials(true)
 			.exposedHeaders(HttpHeaders.LOCATION);
 	}
