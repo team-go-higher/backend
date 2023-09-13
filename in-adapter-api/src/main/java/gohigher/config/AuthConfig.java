680f6196458b4e811d9310e256046cd791ad475d
Dongseok Shin <70707629+shindong96@users.noreply.github.com>
2023-09-14 01:51:15 +0900

    [FEATURE] CORS 설정 (#66)
    * feat: cors 설정
    
    * refactor: 딜리미터 상수화
    
    * refactor: 프론트 도메인 설정

--- in-adapter-api/src/main/java/gohigher/config/AuthConfig.java
+++ in-adapter-api/src/main/java/gohigher/config/AuthConfig.java
@@ -2,17 +2,38 @@ package gohigher.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
-@RequiredArgsConstructor
 public class AuthConfig implements WebMvcConfigurer {
 
+	private static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+	private static final String SPLIT_REGEX = ",";
+
 	private final List<HandlerMethodArgumentResolver> resolvers;
+	private final String[] allowedOrigin;
+
+	public AuthConfig(List<HandlerMethodArgumentResolver> resolvers,
+		@Value("${cors-config.allowed-origin}") String allowedOrigin,
+		@Value("${cors-config.local-origin}") String localOrigin) {
+		this.resolvers = resolvers;
+		this.allowedOrigin = new String[] {allowedOrigin, localOrigin};
+
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins(allowedOrigin)
+			.allowedMethods(ALLOWED_METHOD_NAMES.split(SPLIT_REGEX))
+			.allowCredentials(true)
+			.exposedHeaders(HttpHeaders.LOCATION);
+	}
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
