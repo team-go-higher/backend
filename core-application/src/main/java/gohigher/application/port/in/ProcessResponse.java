dbb2d59d86821650b935e6a177c90aaee9c19ede
shindong96 <2006peter@naver.com>
2023-09-14 23:25:02 +0900

    refactor: LocalDateTime 반환 형식 수정

--- core-application/src/main/java/gohigher/application/port/in/ProcessResponse.java
+++ core-application/src/main/java/gohigher/application/port/in/ProcessResponse.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class ProcessResponse {
 
-	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DatePattern.DATE.getFormat());
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DatePattern.DATE_TIME.getFormat());
 
 	private final Long id;
 	private final String type;
