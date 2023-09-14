f5cd3610bb5118179abe003d2b963d4d0b44bb61
Seongwon97 <osw970325@naver.com>
2023-09-15 01:36:36 +0900

    feat: 지원서 단건 조회 반환 객체 정적 팩터리 메서드 구현

--- core-application/src/main/java/gohigher/application/port/in/ApplicationResponse.java
+++ core-application/src/main/java/gohigher/application/port/in/ApplicationResponse.java
@@ -1,9 +1,9 @@
 package gohigher.application.port.in;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.IntStream;
 
-import gohigher.common.EmploymentType;
+import gohigher.application.Application;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -20,12 +20,42 @@ public class ApplicationResponse {
 	private final String position;
 	private final String jobDescription;
 	private final String workType;
-	private final EmploymentType employmentType;
+	private final String employmentType;
 	private final String careerRequirement;
 	private final String requiredCapability;
 	private final String preferredQualification;
-	private final LocalDateTime deadline;
 	private final List<ProcessResponse> processes;
 	private final int currentProcessOrder;
 	private final String url;
+
+	public static ApplicationResponse from(Application application) {
+		List<ProcessResponse> processResponses = application.getProcesses()
+			.stream()
+			.map(ProcessResponse::from)
+			.toList();
+
+		int currentProcessOrder = IntStream.range(0, application.getProcesses().size())
+			.filter(i -> application.getProcesses().get(i).equals(application.getCurrentProcess()))
+			.findFirst()
+			.orElse(-1);
+
+		return new ApplicationResponse(
+			application.getId(),
+			application.getCompanyName(),
+			application.getTeam(),
+			application.getLocation(),
+			application.getContact(),
+			application.getDuty(),
+			application.getPosition(),
+			application.getJobDescription(),
+			application.getWorkType(),
+			application.getEmploymentType().name(),
+			application.getCareerRequirement(),
+			application.getRequiredCapability(),
+			application.getPreferredQualification(),
+			processResponses,
+			currentProcessOrder,
+			application.getUrl()
+		);
+	}
 }
--- core-application/src/main/java/gohigher/application/port/in/ProcessResponse.java
+++ core-application/src/main/java/gohigher/application/port/in/ProcessResponse.java
@@ -2,6 +2,7 @@ package gohigher.application.port.in;
 
 import java.time.LocalDateTime;
 
+import gohigher.common.Process;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -13,4 +14,9 @@ public class ProcessResponse {
 	private final String type;
 	private final String description;
 	private final LocalDateTime schedule;
+
+	public static ProcessResponse from(Process process) {
+		return new ProcessResponse(process.getId(), process.getType().name(), process.getDescription(),
+			process.getSchedule());
+	}
 }
