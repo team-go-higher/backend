552b7392a0e70ff9a525ddb14ed105e07318effb
Chris <bjuuuu98@gmail.com>
2023-09-15 14:03:51 +0900

    refactor: RequestParam들을 ModelAttribute로 받도록 변경

--- in-adapter-api/src/main/java/gohigher/application/ApplicationProcessQueryController.java
+++ in-adapter-api/src/main/java/gohigher/application/ApplicationProcessQueryController.java
@@ -4,27 +4,27 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
+import gohigher.application.port.in.ApplicationProcessByProcessTypeRequest;
 import gohigher.application.port.in.ApplicationProcessByProcessTypeResponse;
 import gohigher.application.port.in.ApplicationProcessQueryPort;
 import gohigher.auth.support.Login;
-import gohigher.common.ProcessType;
 import gohigher.controller.response.GohigherResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-public class ApplicationProcessQueryController {
+public class ApplicationProcessQueryController implements ApplicationProcessQueryControllerDocs {
 
 	private final ApplicationProcessQueryPort applicationProcessQueryPort;
 
-	@GetMapping("/v1/applications/{applicationId}/processes?type={processType}")
+	@GetMapping("/v1/applications//processes?applicationId={applicationId}&processType={processType}")
 	public ResponseEntity<GohigherResponse<List<ApplicationProcessByProcessTypeResponse>>> getApplicationProcessesByApplicationIdAndType(
-		@Login Long userId, @PathVariable Long applicationId, ProcessType type) {
+		@Login Long userId, @ModelAttribute ApplicationProcessByProcessTypeRequest request) {
 		GohigherResponse<List<ApplicationProcessByProcessTypeResponse>> body = GohigherResponse.success(
-			applicationProcessQueryPort.findByApplicationIdAndProcessType(userId, applicationId, type));
+			applicationProcessQueryPort.findByApplicationIdAndProcessType(userId, request));
 		return ResponseEntity.ok(body);
 	}
 }
