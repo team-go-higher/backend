b44011ebd2a842a037874e6b610fab5294b3f41f
Chris <bjuuuu98@gmail.com>
2023-09-15 13:09:23 +0900

    feat: 지원서 id와 전형 타입에 따른 전형들을 찾는 db out-port 정의

--- core-application/src/main/java/gohigher/application/port/out/persistence/ApplicationProcessPersistenceQueryPort.java
+++ core-application/src/main/java/gohigher/application/port/out/persistence/ApplicationProcessPersistenceQueryPort.java
@@ -1,6 +1,13 @@
 package gohigher.application.port.out.persistence;
 
+import java.util.List;
+
+import gohigher.common.Process;
+import gohigher.common.ProcessType;
+
 public interface ApplicationProcessPersistenceQueryPort {
 
     boolean existsById(Long id);
+
+    List<Process> findByApplicationIdAndProcessType(Long applicationId, ProcessType processType);
 }
