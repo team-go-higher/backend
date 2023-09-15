1ee36c9befe846b232bcac2128aa06132a780ee7
Chris <bjuuuu98@gmail.com>
2023-09-15 13:34:04 +0900

    feat: 다른 사용자의 지원서 전형에 접근하려고 할 때, 예외 발생하도록 구현

--- core-application/src/main/java/gohigher/application/port/in/ApplicationProcessQueryService.java
+++ core-application/src/main/java/gohigher/application/port/in/ApplicationProcessQueryService.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import gohigher.application.ApplicationErrorCode;
+import gohigher.application.port.out.persistence.ApplicationPersistenceQueryPort;
 import gohigher.application.port.out.persistence.ApplicationProcessPersistenceQueryPort;
 import gohigher.common.ProcessType;
+import gohigher.global.exception.GoHigherException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,14 +17,22 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class ApplicationProcessQueryService implements ApplicationProcessQueryPort {
 
+	private final ApplicationPersistenceQueryPort applicationPersistenceQueryPort;
 	private final ApplicationProcessPersistenceQueryPort applicationProcessPersistenceQueryPort;
 
 	@Override
-	public List<ApplicationProcessByProcessTypeResponse> findByApplicationIdAndProcessType(Long applicationId,
-		ProcessType processType) {
+	public List<ApplicationProcessByProcessTypeResponse> findByApplicationIdAndProcessType(Long userId,
+		Long applicationId, ProcessType processType) {
+		validateAuthorizationOfUser(userId, applicationId);
 		return applicationProcessPersistenceQueryPort.findByApplicationIdAndProcessType(applicationId, processType)
 			.stream()
 			.map(ApplicationProcessByProcessTypeResponse::from)
 			.toList();
+	}
+
+	private void validateAuthorizationOfUser(Long userId, Long applicationId) {
+		if(!applicationPersistenceQueryPort.existsByIdAndUserId(applicationId, userId)) {
+			throw new GoHigherException(ApplicationErrorCode.APPLICATION_NOT_FOUND);
+		}
 	}
 }
---
+++ core-application/src/test/java/gohigher/application/port/in/ApplicationProcessQueryServiceTest.java
@@ -0,0 +1,59 @@
+package gohigher.application.port.in;
+
+import static gohigher.application.ApplicationErrorCode.*;
+import static gohigher.common.ProcessType.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import gohigher.application.port.out.persistence.ApplicationPersistenceQueryPort;
+import gohigher.application.port.out.persistence.ApplicationProcessPersistenceQueryPort;
+
+@DisplayName("ApplicationProcessQueryService 클래스의")
+@ExtendWith(MockitoExtension.class)
+class ApplicationProcessQueryServiceTest {
+
+	@Mock
+	private ApplicationPersistenceQueryPort applicationPersistenceQueryPort;
+	@Mock
+	private ApplicationProcessPersistenceQueryPort applicationProcessPersistenceQueryPort;
+	@InjectMocks
+	private ApplicationProcessQueryService applicationProcessQueryService;
+
+	@DisplayName("findByApplicationIdAndProcessType 메서드는")
+	@Nested
+	class Describe_FindByApplicationIdAndProcessType {
+
+		@DisplayName("id와 userId가 일치하는 지원서가 없는 경우에, ")
+		@Nested
+		class Context_NonExistent_Application_By_Id_And_User_Id {
+
+			private final Long userId = 2L;
+			private final Long applicationId = 3L;
+
+			@BeforeEach
+			void setUp() {
+				when(applicationPersistenceQueryPort.existsByIdAndUserId(applicationId, userId))
+					.thenReturn(false);
+			}
+
+			@DisplayName("예외를 발생시킨다.")
+			@Test
+			void it_Throws_Application_Not_Found_Exception() {
+				//when then
+				assertThatThrownBy(() ->
+					applicationProcessQueryService.findByApplicationIdAndProcessType(userId, applicationId, TEST))
+					.hasMessage(APPLICATION_NOT_FOUND.getMessage());
+			}
+		}
+	}
+
+}
