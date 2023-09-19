a8c7b372a799707b9f0bd6d091f3fb4066ef8959
Chris <bjuuuu98@gmail.com>
2023-09-19 14:38:49 +0900

    feat: 직무 전체 조회 API 구현

---
+++ in-adapter-api/src/main/java/gohigher/position/PositionQueryController.java
@@ -0,0 +1,25 @@
+package gohigher.position;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gohigher.controller.response.GohigherResponse;
+import gohigher.position.port.in.PositionQueryPort;
+import gohigher.position.port.in.PositionResponse;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class PositionQueryController {
+
+	private final PositionQueryPort positionQueryPort;
+
+	@GetMapping("/v1/positions")
+	public ResponseEntity<GohigherResponse<List<PositionResponse>>> getPositions() {
+		List<PositionResponse> response = positionQueryPort.findPositions();
+		return ResponseEntity.ok(GohigherResponse.success(response));
+	}
+}
