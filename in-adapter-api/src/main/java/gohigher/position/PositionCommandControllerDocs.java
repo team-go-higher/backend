f7b76fd49d6fc7f574260a617c2944509189e64c
shindong96 <2006peter@naver.com>
2023-09-22 21:10:58 +0900

    docs: swagger 명세 작성

---
+++ in-adapter-api/src/main/java/gohigher/position/PositionCommandControllerDocs.java
@@ -0,0 +1,35 @@
+package gohigher.position;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import gohigher.position.port.in.DesiredPositionRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "직무")
+public interface PositionCommandControllerDocs {
+
+	@Operation(summary = "희망 직무 등록")
+	@ApiResponses(value = {
+		@ApiResponse(responseCode = "204", description = "희망 직무 등록 성공"),
+		@ApiResponse(responseCode = "400", description = "희망 직무가 이미 게시된 직무 목록에 있음", content = @Content(
+			examples = {
+				@ExampleObject(name = "직무가 이미 게시되어 있음", value = """
+					{
+					"success": false,
+					"error": {
+						"code": "POSITION_011",
+						"message": "목록에 존재하는 포지션입니다."
+					},
+					"data": null
+					}
+					""")}))})
+	ResponseEntity<Void> saveDesiredPositions(@Parameter(hidden = true) Long userId,
+		@RequestBody DesiredPositionRequest request);
+}
