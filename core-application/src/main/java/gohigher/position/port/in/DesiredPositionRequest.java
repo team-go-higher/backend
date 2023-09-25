b50317d0e0f654664d9b75992941c825be2e449f
shindong96 <2006peter@naver.com>
2023-09-25 16:55:21 +0900

    refactor: Validate 간편화

--- core-application/src/main/java/gohigher/position/port/in/DesiredPositionRequest.java
+++ core-application/src/main/java/gohigher/position/port/in/DesiredPositionRequest.java
@@ -4,16 +4,21 @@ import java.util.List;
 
 import org.springframework.util.CollectionUtils;
 
+import jakarta.validation.constraints.AssertTrue;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @Getter
-@DesiredPositionBlank(existedPositionIds = "existedPositionIds", personalPositions = "personalPositions")
 public class DesiredPositionRequest {
 
 	private List<Long> existedPositionIds;
 	private List<String> personalPositions;
+
+	@AssertTrue(message = "GLOBAL_011||빈 입력값입니다.")
+	public boolean isValidInput() {
+		return !CollectionUtils.isEmpty(existedPositionIds) || !CollectionUtils.isEmpty(personalPositions);
+	}
 
 	public boolean isExistedPositionIdsEmpty() {
 		return CollectionUtils.isEmpty(existedPositionIds);
