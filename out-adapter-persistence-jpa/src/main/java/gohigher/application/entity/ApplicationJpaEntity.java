d37cc76a1d6e5a21b7f6aecbe847666eb32e6927
Seongwon97 <osw970325@naver.com>
2023-09-26 13:35:10 +0900

    feat: 지원서 Entity의 업데이트 메서드 생성

--- out-adapter-persistence-jpa/src/main/java/gohigher/application/entity/ApplicationJpaEntity.java
+++ out-adapter-persistence-jpa/src/main/java/gohigher/application/entity/ApplicationJpaEntity.java
@@ -133,4 +133,21 @@ public class ApplicationJpaEntity {
 			workType, employmentType, careerRequirement, requiredCapability, preferredQualification,
 			new Processes(processes), url, currentProcess);
 	}
+
+	public void update(Application application) {
+		this.companyName = application.getCompanyName();
+		this.team = application.getTeam();
+		this.location = application.getLocation();
+		this.contact = application.getContact();
+		this.position = application.getPosition();
+		this.specificPosition = application.getSpecificPosition();
+		this.jobDescription = application.getJobDescription();
+		this.workType = application.getWorkType();
+		this.employmentType = application.getEmploymentType();
+		this.careerRequirement = application.getCareerRequirement();
+		this.requiredCapability = application.getRequiredCapability();
+		this.preferredQualification = application.getPreferredQualification();
+		this.url = application.getUrl();
+		this.currentProcessOrder = application.getCurrentProcess().getOrder();
+	}
 }
