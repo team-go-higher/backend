27e497979d7b96495232c4f4155ff3f796018190
Chris <bjuuuu98@gmail.com>
2023-10-06 21:59:18 +0900

    refactor: 전형일 필수값에서 옵션 값으로 변경

--- core-application/src/main/java/gohigher/application/port/in/SimpleApplicationProcessRequest.java
+++ core-application/src/main/java/gohigher/application/port/in/SimpleApplicationProcessRequest.java
@@ -19,7 +19,6 @@ public class SimpleApplicationProcessRequest {
 	@NotBlank(message = "JOB_INFO_006||세부 전형이 입력되지 않았습니다.")
 	private String description;
 
-	@NotNull(message = "JOB_INFO_007||전형 일정이 입력되지 않았습니다.")
 	private LocalDateTime schedule;
 
 	public Process toDomain() {
