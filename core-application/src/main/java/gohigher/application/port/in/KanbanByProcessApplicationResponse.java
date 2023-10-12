7c47639491a6b445bae83ba94d47b6024c6ad450
yukong <kyb062666@gmail.com>
2023-10-12 18:56:55 +0900

    feat: 칸반 전형별 목록 조회 service 구현

---
+++ core-application/src/main/java/gohigher/application/port/in/KanbanByProcessApplicationResponse.java
@@ -0,0 +1,30 @@
+package gohigher.application.port.in;
+
+import java.time.LocalDateTime;
+
+import gohigher.application.Application;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class KanbanByProcessApplicationResponse {
+
+	private final long id;
+	private final String companyName;
+	private final String position;
+	private final String specificPosition;
+	private final String processDescription;
+	private final LocalDateTime schedule;
+
+	public static KanbanByProcessApplicationResponse from(Application application) {
+		return new KanbanByProcessApplicationResponse(
+			application.getId(),
+			application.getCompanyName(),
+			application.getPosition(),
+			application.getSpecificPosition(),
+			application.getCurrentProcess().getDescription(),
+			application.getCurrentProcess().getSchedule()
+		);
+	}
+}
