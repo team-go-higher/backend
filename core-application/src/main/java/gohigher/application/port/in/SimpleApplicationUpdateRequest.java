de750a48e3defa12e5881183389107c6937df5f6
Seongwon97 <osw970325@naver.com>
2023-10-24 21:51:09 +0900

    feat: 지원서 간편수정 in-port 지정

---
+++ core-application/src/main/java/gohigher/application/port/in/SimpleApplicationUpdateRequest.java
@@ -0,0 +1,25 @@
+package gohigher.application.port.in;
+
+import java.time.LocalDateTime;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class SimpleApplicationUpdateRequest {
+
+	@NotBlank(message = "JOB_INFO_002||회사명이 입력되지 않았습니다.")
+	private String companyName;
+
+	@NotBlank(message = "JOB_INFO_003||직무가 입력되지 않았습니다.")
+	private String position;
+
+	@NotBlank(message = "APPLICATION_004||지원서의 전형 id가 입력되지 않았습니다.")
+	private Long processId;
+
+	private LocalDateTime schedule;
+
+	private String url;
+}
