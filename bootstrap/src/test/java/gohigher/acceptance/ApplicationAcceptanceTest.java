600db678a2722298cf5867f1e7167d3a5af06802
shindong96 <2006peter@naver.com>
2023-10-30 18:03:42 +0900

    test: e2e Test 구조 설정 및 예시 API 테스트 작성

---
+++ bootstrap/src/test/java/gohigher/acceptance/ApplicationAcceptanceTest.java
@@ -0,0 +1,48 @@
+package gohigher.acceptance;
+
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import gohigher.application.port.in.SimpleApplicationProcessRequest;
+import gohigher.application.port.in.SimpleApplicationRequest;
+import gohigher.common.ProcessType;
+import gohigher.user.auth.Provider;
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+
+public class ApplicationAcceptanceTest extends AcceptanceTest {
+
+	@DisplayName("지원서를 작성한다")
+	@Test
+	void create_application() {
+		// given
+		String accessToken = signUp("azpi@email.com", Provider.GOOGLE);
+
+		SimpleApplicationProcessRequest processRequest = new SimpleApplicationProcessRequest(
+			ProcessType.TO_APPLY.name(),
+			"지원 예정", null);
+		SimpleApplicationRequest simpleApplicationRequest = new SimpleApplicationRequest("카카오", "개발자", "url",
+			processRequest);
+
+		// when
+		ValidatableResponse response = RestAssured.given().log().all()
+			.auth().oauth2(accessToken)
+			.body(simpleApplicationRequest)
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.when().post("/v1/applications/simple")
+			.then().log().all();
+
+		// then
+		response.statusCode(HttpStatus.OK.value())
+			.body("success", equalTo(true))
+			.body("data.id", notNullValue())
+			.body("data.companyName", equalTo(simpleApplicationRequest.getCompanyName()))
+			.body("data.currentProcessSchedule", equalTo(processRequest.getSchedule()))
+			.body("data.currentProcessDescription", equalTo(processRequest.getDescription()));
+	}
+}
--- core-application/src/main/java/gohigher/application/port/in/SimpleApplicationProcessRequest.java
+++ core-application/src/main/java/gohigher/application/port/in/SimpleApplicationProcessRequest.java
@@ -5,12 +5,13 @@ import java.time.LocalDateTime;
 import gohigher.common.Process;
 import gohigher.common.ProcessType;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class SimpleApplicationProcessRequest {
 
 	@NotBlank(message = "JOB_INFO_005||전형 단계가 입력되지 않았습니다.")
