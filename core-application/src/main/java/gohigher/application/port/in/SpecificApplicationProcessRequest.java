34145320973b7f22c2b0439115b11573d36193aa
Seongwon97 <osw970325@naver.com>
2023-10-30 16:41:57 +0900

    refactor: 상세 등록의 ProcessRequest의 일정에 붙은 NotNull 조건 제거

--- core-application/src/main/java/gohigher/application/port/in/SpecificApplicationProcessRequest.java
+++ core-application/src/main/java/gohigher/application/port/in/SpecificApplicationProcessRequest.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import gohigher.common.Process;
 import gohigher.common.ProcessType;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,7 +16,7 @@ public class SpecificApplicationProcessRequest {
 	private String type;
 	@NotBlank(message = "JOB_INFO_006||세부 전형이 입력되지 않았습니다.")
 	private String description;
-	@NotNull(message = "JOB_INFO_007||전형 일정이 입력되지 않았습니다.")
+
 	private LocalDateTime schedule;
 
 	public Process toDomain() {
