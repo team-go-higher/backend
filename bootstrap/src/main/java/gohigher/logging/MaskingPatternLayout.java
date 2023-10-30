6a73a28087a8c83b298507904615c45aa6147bc1
shindong96 <2006peter@naver.com>
2023-10-31 02:17:37 +0900

    feat: logging 처리

---
+++ bootstrap/src/main/java/gohigher/logging/MaskingPatternLayout.java
@@ -0,0 +1,49 @@
+package gohigher.logging;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class MaskingPatternLayout extends PatternLayout {
+
+	private static final String MASKED_STRING = "********";
+
+	private Pattern multilinePattern;
+	private final List<String> maskPatterns = new ArrayList<>();
+
+	public void addMaskPattern(final String maskPattern) {
+		maskPatterns.add(maskPattern);
+		multilinePattern = Pattern.compile(String.join("|", maskPatterns), Pattern.CASE_INSENSITIVE);
+	}
+
+	@Override
+	public String doLayout(final ILoggingEvent event) {
+		return maskMessage(super.doLayout(event));
+	}
+
+	private String maskMessage(final String message) {
+		if (multilinePattern == null) {
+			return message;
+		}
+
+		final StringBuilder stringBuilder = new StringBuilder(message);
+		final Matcher matcher = multilinePattern.matcher(stringBuilder);
+		while (matcher.find()) {
+			maskIfMatch(stringBuilder, matcher);
+		}
+
+		return stringBuilder.toString();
+	}
+
+	private void maskIfMatch(final StringBuilder stringBuilder, final Matcher matcher) {
+		IntStream.rangeClosed(1, matcher.groupCount())
+			.filter(it -> !Objects.isNull(matcher.group(it)))
+			.forEach(it -> stringBuilder.replace(matcher.start(it), matcher.end(it), MASKED_STRING));
+	}
+}
---
+++ out-adapter-persistence-jpa/src/main/java/gohigher/logging/HibernateConfig.java
@@ -0,0 +1,22 @@
+package gohigher.logging;
+
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HibernateConfig {
+
+	private final QueryCountInspector queryCountInspector;
+
+	public HibernateConfig(final QueryCountInspector queryCountInspector) {
+		this.queryCountInspector = queryCountInspector;
+	}
+
+	@Bean
+	public HibernatePropertiesCustomizer configureStatementInspector() {
+		return hibernateProperties ->
+			hibernateProperties.put(AvailableSettings.STATEMENT_INSPECTOR, queryCountInspector);
+	}
+}
