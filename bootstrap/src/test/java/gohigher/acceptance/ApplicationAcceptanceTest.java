6d318613decf930ce7319d385bcae94a1448feac
yukong <kyb062666@gmail.com>
2024-01-11 17:21:17 +0900

    test: 지원서 삭제 인수 테스트 작성

--- bootstrap/src/test/java/gohigher/acceptance/ApplicationAcceptanceTest.java
+++ bootstrap/src/test/java/gohigher/acceptance/ApplicationAcceptanceTest.java
@@ -15,7 +15,7 @@ import io.restassured.response.ValidatableResponse;
 @DisplayName("Application 인수테스트의")
 public class ApplicationAcceptanceTest extends AcceptanceTest {
 
-	@DisplayName("지원서를 작성 기능을 테스트한다")
+	@DisplayName("지원서를 작성하는 기능을 테스트한다")
 	@Test
 	void create_application() {
 		// given
@@ -37,5 +37,29 @@ public class ApplicationAcceptanceTest extends AcceptanceTest {
 			.body("data.companyName", equalTo(simpleApplicationRequest.getCompanyName()))
 			.body("data.currentProcessSchedule", equalTo(processRequest.getSchedule()))
 			.body("data.currentProcessDescription", equalTo(processRequest.getDescription()));
+	}
+
+	@DisplayName("지원서를 삭제하는 기능을 테스트한다")
+	@Test
+	void delete_application() {
+		// given
+		String accessToken = signUp("azpi@email.com", Provider.GOOGLE);
+		int applicationId = createApplication(accessToken);
+
+		// when
+		ValidatableResponse response = delete(accessToken, "/v1/applications/" + applicationId);
+
+		// then
+		response.statusCode(HttpStatus.NO_CONTENT.value());
+	}
+
+	private int createApplication(String accessToken) {
+		SimpleApplicationRequest request = new SimpleApplicationRequest(
+			"카카오", "개발자", "url",
+			new SimpleApplicationProcessRequest(ProcessType.TO_APPLY.name(), "지원 예정", null));
+
+		ValidatableResponse response = post(accessToken, "/v1/applications/simple", request);
+
+		return response.extract().path("data.id");
 	}
 }
