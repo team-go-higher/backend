35013cf7da884d21d85d271109e666ef00177ed7
shindong96 <2006peter@naver.com>
2024-02-19 20:36:52 +0900

    refactor: EmploymentType null 값 처리 추가

--- core-domain/src/main/java/gohigher/common/EmploymentType.java
+++ core-domain/src/main/java/gohigher/common/EmploymentType.java
@@ -11,16 +11,25 @@ public enum EmploymentType {
 	INTERN,
 	PERMANENT,
 	TEMPORARY,
+	UNDEFINED,
 	;
 
 	public static EmploymentType from(String value) {
 		if (value == null) {
-			return null;
+			return UNDEFINED;
 		}
 
 		return Arrays.stream(values())
 			.filter(employmentType -> employmentType.name().equals(value))
 			.findAny()
 			.orElseThrow(() -> new GoHigherException(INVALID_EMPLOYMENT_TYPE));
+	}
+
+	public String getValue() {
+		if (this.equals(UNDEFINED)) {
+			return null;
+		}
+
+		return this.name();
 	}
 }
