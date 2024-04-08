f102ddf70c4b64dac81b8f418615da04811162f1
shindong96 <2006peter@naver.com>
2024-04-08 17:35:34 +0900

    chore: deploy.sh 내용 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,13 +4,15 @@ TEST_PORT=8081
 DEV_PROFILE="dev"
 TEST_PROFILE="test"
 
+DEPLOY_FOLDER="gohigher"
 JAR_FILE="bootstrap-0.0.1-SNAPSHOT.jar"
+
 TEST_FOLDER="test"
 DEV_LOG_FILE="gohigher.out"
 TEST_LOG_FILE="gohigher-test.out"
 
 cd ~
-cd
+cd ${DEPLOY_FOLDER}
 
 echo "> kill spring port"
 fuser -k -n tcp ${DEV_PORT}
