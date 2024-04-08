a7951351b863dd3159f6e4a5f08cee4c44c91cca
Dongseok Shin <70707629+shindong96@users.noreply.github.com>
2024-04-08 20:54:18 +0900

    [INFRA] CD 구축 (#197)
    * feat: cd 과정에 암호화 키 동적 변수 주입
    
    * fix: 변수 주입 수정
    
    * fix: 변수 주입 로직 수정
    
    * cd: zip으로 압축
    
    * cd: aws s3에 zip 올리고 codeDeploy 실행
    
    * chore: code deploy를 위한 파일 생성
    
    * chore: deploy.sh 빈 파일 생성
    
    * fix: 오타 수정
    
    * chore: deploy.sh 내용 작성
    
    * chore: deploy.sh 내용 작성
    
    * chore: deploy.sh 내용 수정
    
    * chore: deploy.sh 내용 수정
    
    * cd: build가 아닌 bootJar로 수정
    
    * chore: 파일 이동 수정
    
    * chore: 파일 이동 수정
    
    * chore: 파일 이동전 시간 차
    
    * cd: develop 브랜치 push 상황에서만 실행
    
    * style: 인라인

---
+++ scripts/deploy.sh
@@ -0,0 +1,30 @@
+DEV_PORT=8080
+TEST_PORT=8081
+
+DEV_PROFILE="dev"
+TEST_PROFILE="test"
+
+DEPLOY_FOLDER="gohigher"
+JAR_FILE="bootstrap-0.0.1-SNAPSHOT.jar"
+
+TEST_FOLDER="test"
+DEV_LOG_FILE="gohigher.out"
+TEST_LOG_FILE="gohigher-test.out"
+
+cd ~
+cd ${DEPLOY_FOLDER}
+
+echo "> kill spring port"
+fuser -k -n tcp ${DEV_PORT}
+fuser -k -n tcp ${TEST_PORT}
+
+echo "> execute dev jar"
+cd bootstrap/build/libs
+nohup java -jar -Dspring.profiles.active=${DEV_PROFILE} ${JAR_FILE} 1>${DEV_LOG_FILE} 2>&1 &
+sleep 3
+
+echo "> execute test jar"
+mkdir ${TEST_FOLDER}
+sudo mv ${JAR_FILE} ${TEST_FOLDER}/
+cd ${TEST_FOLDER}
+nohup java -jar -Dspring.profiles.active=${TEST_PROFILE} -Dserver.port=${TEST_PORT} ${JAR_FILE} 1>${TEST_LOG_FILE} 2>&1 &
